1ed9c0697daa3bb7a5ede1f72b8e08e313121e61
Brad Chitwood <chitwoob@gmail.com>
2019-07-24 19:48:51 -0400

    Updated getContacts

--- ClientApp/src/api/contactapi.js
+++ ClientApp/src/api/contactapi.js
@@ -1,18 +1,5 @@
 export async function getContactsApi() {
-  // const url = `api/Tracker/AddTracker`;
 
-  // let headers = new Headers();
-  // const jsonStr = JSON.stringify(hwObj);
-
-  // headers.append('Content-Type', 'text/json');
-  // headers.set('Authorization', 'Basic ' + Buffer.from(username + ":" + password).toString('base64'));
-
-  // try {
-  //   const response = await fetch(url, { method: 'POST', headers: headers, body: jsonStr });
-  //   return await response.json();
-  // } catch (error) {
-  //   return {};
-  // }
 }
 
 export async function addContactApi(data) {
